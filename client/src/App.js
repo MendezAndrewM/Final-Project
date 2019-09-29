03965841e2c981a76f81ec804edd9c2d26e82857
AndrewM <Mendez.AndrewM@gmail.com>
2019-09-29 14:48:29 -0700

    create-react-app

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          This text is returned from App.js
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
