b76199ab039771d876787e0102888b188dcff489
jake hanes <jhanesx@yahoo.com>
2019-10-03 12:48:41 -0700

    Nav bar made materialize stuff put into json npm install materilize things

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Navbar, NavItem, Dropdown } from 'react-materialize'
+
+const Nav = () => {
+    <Navbar brand={<a />} alignLinks="right">
+    <NavItem href="">
+    Getting started
+    </NavItem>
+    <NavItem href="components.html">
+    Components
+    </NavItem>
+    <Dropdown trigger={<a />}>
+    <a href="#">
+    one
+    </a>
+    <a href="#">
+    two
+    </a>
+    <Divider/>
+    <a href="#">
+    three
+    </a>
+    </Dropdown>
+    </Navbar>
+}
+
+export default Nav
