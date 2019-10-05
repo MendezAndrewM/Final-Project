73c2e8a5336c7afd2ee0b5ba0856110ac705dca2
unknown <mendez.andrewm@gmail.com>
2019-10-04 20:12:52 -0700

    Started Home page

---
+++ client/src/pages/Home.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+// import API from "../utils/API";
+// import { Link } from "react-router-dom";
+// import Nav from "../components/Nav";
+// import Footer from "../components/Footer";
+// import Jumbotron from "../components/Jumbotron";
+// import { Col, Row, Container } from "../components/Grid";
+
+class Home extends Component {
+    // state = {
+    //     key: value
+    // }
+
+    // componentDidMount() {
+    //     reRenderThingsHereIThink();
+    // }
+
+    /*
+        ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+                            API Calls will go Here
+        ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+    */
+
+    render() {
+    return (
+            <div>
+                <h3>About</h3>
+                <p> Hodor, hodor, hodor hodor. Hodor, hodor. Hodor. Hodor. HODOR 
+                    hodor, hodor. HODOR HODOR! Hodor! Hodor hodor. Hodor hodor. 
+                    Hodor hodor! Hodor? Hodor hodor! HODOR? HODOR HODOR! Hodor 
+                    hodor! Hodor... Hodor hodor. Hodor hodor!
+
+                    Hodor hodor! HODOR HODOR! HODOR HODOR! Hodor? Hodor, hodor. 
+                    Hodor. Hodor. Hodor, hodor. Hodor. Hodor! Hodor hodor. Hodor, 
+                    hodor. Hodor. Hodor, hodor, hodor hodor. Hodor? HODOR HODOR! 
+                    Hodor, hodor. Hodor. Hodor... Hodor hodor. Hodor, hodor, hodor 
+                    hodor. Hodor... Hodor hodor.
+                </p>
+                <p>Here can be a link to sign in</p>
+
+                <p>Here can be a list of featured businesses in the area</p>
+             </div>
+        )
+    }
+}
+
+export default Home;
