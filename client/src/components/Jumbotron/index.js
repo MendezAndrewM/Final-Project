4a20a94672ab9f74150d50a49ffe3550105f1d9a
unknown <mendez.andrewm@gmail.com>
2019-10-08 21:39:08 -0700

    Added Search bar component to jumbotron

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -1,11 +1,15 @@
 import React from "react";
-import "./style.css"
+import "./style.css";
+import SearchBar from "../SearchBar";
 
 
 const Jumbotron = ({ children }) =>{
     return(
         <div className="jumbo">
             
+            <h1>The Thing</h1>
+            <h5>And some stuff about the thing</h5>
+            <SearchBar />
             {children}
             
         </div>
