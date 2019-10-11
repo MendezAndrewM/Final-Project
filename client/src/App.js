5d01af1c59ed8af87838a4c84e3f10b0690e29ff
unknown <mendez.andrewm@gmail.com>
2019-10-10 19:59:45 -0700

    stuff

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,14 @@ import Nav from "./components/Nav/index";
 import Footer from "./components/Footer/index";
 import Jumbotron from "./components/Jumbotron/index";
 import Card from "./components/Card/index";
+import BusinessPage from "./pages/BusinessPage"
 
 function App() {
   return (
     <div className="App">
       <Nav />
       {/* React Router Here */}
+      <BusinessPage />
       <Footer />
     </div>
   );
--- client/src/pages/BusinessPage.js
+++ client/src/pages/BusinessPage.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Row } from "react-materialize";
 
-const rating = 3.5 //Only Here as placeholder. Rating will be pulled from API 
+const rating = 0.2 //Only Here as placeholder. Rating will be pulled from API 
 
 class BusinessPage extends Component {
     state = {
@@ -14,15 +14,16 @@ class BusinessPage extends Component {
     // Functions to add comments / ratings / reviews
     // via API calls 
 
+
     displayStars = (num) => {
         const whole = Math.floor(num);
         const dec = (num - Math.floor(num)); 
-        const stars = ""
-        for (let i=0; i<whole; i++) { stars += "star " }
-        if (dec > 0.3) { star += "star_half" }
+        let stars = "";
+        for (let i=0; i<whole; i++) { stars += "star " };
+        if (dec > 0.3) { stars += "star_half" };
+        if (num < 0.3) { stars += "star_half" }; //Sets minimum rating as half_star
         return stars;
-    }
-
+    };
 
 
     render() {
@@ -30,13 +31,14 @@ class BusinessPage extends Component {
             <div className="wrapper">
 
                 <Row>
-                    <div className="bizBox col s10 m5 offset-m2 offset-s1">
+                    <div className="bizBox col s10 m4 offset-m2 offset-s1">
                         <h3>Name of Business here</h3>
                         <p>(602)555-5555</p>
                         <p>1234 Fake Blvd, <br />Nowhere, AZ 85001 </p>
                     </div>
-                    <div className="statBoxBox col s10 m5 offset-m2 offset-s1">
+                    <div className="statBoxBox col s10 m4 offset-m2 offset-s1">
                         <span className="material-icons">{this.displayStars(rating)}</span>
+                        <span>Costs {}</span>
                     </div>
                 </Row>
 
