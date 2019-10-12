f62a7deece6a2946c8e065d2288cd1ad83784143
jake hanes <jhanesx@yahoo.com>
2019-10-12 13:59:25 -0700

    utils made for api son

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/Home"
 import Nav from "./components/Nav/index";
 import Footer from "./components/Footer/index";
 import UserPage from "./pages/UserPage";
+import Seeds from "../../scripts/seedsDB"
+import API from "../src/utils/API"
 
 const [loggedin, login] = useState(false);
+const [ratings, rate] = useState(0);
+const [Seeds] = useState(Seeds);
+
+
+
 
 
 
