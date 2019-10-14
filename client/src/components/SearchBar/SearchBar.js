4003c549408a80c719040bbfc938865e1a22f949
AndrewM <Mendez.AndrewM@gmail.com>
2019-10-13 20:17:09 -0700

    Updated Styles for HomePage

--- client/src/components/SearchBar/SearchBar.js
+++ client/src/components/SearchBar/SearchBar.js
@@ -4,12 +4,12 @@ import "./SearchBar.css";
 
 const SearchBar = () => {
     return ( 
-        <Row>
-        <input placeholder="Enter Business or Service"
-            className="col s4 offset-s2"></input>
-        <input placeholder="Enter Zip Code"
+        <Row className="searchBar">
+        <input placeholder="Search Business or Service"
+            className="col s7 m4 offset-m2"></input>
+        <input placeholder="Zip Code"
             className="col s3"></input>
-        <button className="col moop s1 material-icons">search</button>
+        <button className="col moop s2 m1 material-icons">search</button>
         </Row>
     );
 }
