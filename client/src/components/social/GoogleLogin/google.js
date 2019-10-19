2dada0dbf8f7ccf6dab1b9695b7a60efa01a1628
Joey Duchett <joeyduchett@yahoo.com>
2019-10-19 10:52:05 -0700

    added sign in feature, button, components, logic, etc

---
+++ client/src/components/social/GoogleLogin/google.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+
+
+const GoogleSignIn = () => {
+    return (
+        <button id = "customBtn"></button>
+    )
+}
+ export default GoogleSignIn; 
+      
