6ee6b8cd8581ba2f258790e6476b6aeafc179fd5
jake hanes <jhanesx@yahoo.com>
2019-10-22 23:57:41 -0700

    formpage done expect the handle button

--- client/src/components/ReviewForm/index.js
+++ client/src/components/ReviewForm/index.js
@@ -6,8 +6,8 @@ import "./style.css"
 class ReviewForm extends Component {
   render() {
     return (
-      <Row>
-        <Col s={12}>
+      <Row className="bigWrap">
+        <Col s={10} offset="s3">
           <Row>
             <Col s={6} className="input-field">
               <textarea id="textarea1" className="materialize-textarea" data-length="120"></textarea>
@@ -23,23 +23,23 @@ class ReviewForm extends Component {
           <Row>
             <Col s={6} className="input-field">
               <textarea id="textarea2" className="materialize-textarea" data-length="120"></textarea>
-              <label for="textarea2">What did you pay</label>
+              <label for="textarea2">What did you pay?</label>
             </Col>
           </Row>
-          <Row className="wrapper">
+          <Row>
             <Col s={1}>
-              
+
               <div>
                 <label>
-                  <input className="with-gap" value="0" name="group3" type="radio"/>
+                  <input className="with-gap" value="0" name="group3" type="radio" />
                   <span>0</span>
                 </label>
               </div>
-            </Col>  
+            </Col>
             <Col s={1}>
               <div>
                 <label>
-                  <input className="with-gap" value="1" name="group3" type="radio"/>
+                  <input className="with-gap" value="1" name="group3" type="radio" />
                   <span>1</span>
                 </label>
               </div>
@@ -47,7 +47,7 @@ class ReviewForm extends Component {
             <Col s={1}>
               <div>
                 <label>
-                  <input className="with-gap" value="2" name="group3" type="radio"/>
+                  <input className="with-gap" value="2" name="group3" type="radio" />
                   <span>2</span>
                 </label>
               </div>
@@ -55,7 +55,7 @@ class ReviewForm extends Component {
             <Col s={1}>
               <div>
                 <label>
-                  <input className="with-gap" value="3" name="group3" type="radio"/>
+                  <input className="with-gap" value="3" name="group3" type="radio" />
                   <span>3</span>
                 </label>
               </div>
@@ -64,7 +64,7 @@ class ReviewForm extends Component {
 
               <div>
                 <label>
-                  <input className="with-gap" value="4" name="group3" type="radio"/>
+                  <input className="with-gap" value="4" name="group3" type="radio" />
                   <span>4</span>
                 </label>
               </div>
@@ -73,12 +73,20 @@ class ReviewForm extends Component {
 
               <div>
                 <label>
-                  <input className="with-gap" value="5" name="group3" type="radio"/>
+                  <input className="with-gap" value="5" name="group3" type="radio" />
                   <span>5</span>
                 </label>
               </div>
             </Col>
-            
+
+          </Row>
+          <Row>
+            <Col s={3} offset="s4">
+
+              <button className="btn waves-effect waves-light" type="submit" name="action">Submit
+                <i className="material-icons right">send</i>
+              </button>
+            </Col>
           </Row>
         </Col>
       </Row>
