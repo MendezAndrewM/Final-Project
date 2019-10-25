3243a820644938ab40aff0ddbb7f53894a32ff3b
jake hanes <jhanesx@yahoo.com>
2019-10-24 20:53:21 -0700

    search handler half way done

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -3,14 +3,16 @@ import "./style.css";
 import SearchBar from "../SearchBar";
   
 
-const Jumbotron = ({ children }) =>{
+const Jumbotron = (props) =>{
     return(
         <div className="jumbo">
             
             <h1>The Thing</h1>
             <h5>And some stuff about the thing</h5>
-            <SearchBar />
-            {children}
+            <SearchBar 
+            onClick={props.onClick}
+            />
+            
             
         </div>
     )
--- client/src/components/SearchBar/SearchBar.js
+++ client/src/components/SearchBar/SearchBar.js
@@ -2,14 +2,14 @@ import React from "react";
 import { Row } from "react-materialize";
 import "./SearchBar.css";
 
-const SearchBar = () => {
+const SearchBar = (props) => {
     return ( 
         <Row className="searchBar">
         <input placeholder="Search Business or Service"
             className="col s7 m4 offset-m2"></input>
         <input placeholder="Zip Code"
             className="col s3"></input>
-        <button className="col moop s2 m1 material-icons">search</button>
+        <button onClick={props.onClick} className="col moop s2 m1 material-icons">search</button>
         </Row>
     );
 }
