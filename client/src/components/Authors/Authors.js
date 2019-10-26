7bf0af6d95f811465e34f202ff2ba5cab52dee45
jake hanes <jhanesx@yahoo.com>
2019-10-25 22:05:53 -0700

    about page done and footer links changed

--- client/src/components/Authors/Authors.js
+++ client/src/components/Authors/Authors.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import { Row } from "react-materialize";
+import "./Authors.css"
 
 const Authors = props => <>
-    <div>
+    <div className="authorBox">
         <h2>Authors:</h2>
-        
+        <p>Jacob: <a>https://github.com/jacobhanes</a></p>
+        <p>Andrew: <a>https://github.com/MendezAndrewM</a> </p>
+        <p>Joey: <a>https://github.com/joeyduchett</a></p>
     </div>
 </>
 
